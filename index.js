ed0e69f2207f9a03a8dc3eb39d25cfa45016acd0
Joao Medeiros <corona>
2020-10-18 16:57:02 -0300

    setup

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from 'expo'
 
-import App from './App';
+import App from './src/App'
+import TrackPlayer from 'react-native-track-player'
+import TrackPlayerConfig from './src/config/trackPlayer'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(App)
+TrackPlayer.registerPlaybackService(() => TrackPlayerConfig())
